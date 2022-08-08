9cacf604c72dcbbe1e815e3b8d82cba9a81c6b96
sjohnson <stuart.g.johnson@gmail.com>
2022-08-08 16:00:08 -0700

    More testing and mapping tools (folium, for example). Shelving for a while due to complexities of DTW as it applies to the problem at hand.

--- edit_gpx.py
+++ edit_gpx.py
@@ -1,4 +1,74 @@
 import gpxpy as gp
+import numpy as np
+import dtw
+import skimage.measure as measure
+
+
+def patch_deletions_with_template(query: np.ndarray, template: np.ndarray) -> np.ndarray:
+    # compute distance along the template as a reference length
+    delta = template[1:, :] - template[0:-1, :]
+    template_dist = np.sum(np.linalg.norm(delta, axis=1))
+    alignment = dtw.dtw(query, template, keep_internals=True)
+    alignment.plot(type="threeway")
+    # merge these two trajectories into a single trajectory.
+    # first, locate the alignment gaps. These have to be at least
+    # min_dist in euclidean dist to be corrected. We'll do this with
+    # connected component analysis of alignment regions which have zero
+    # progress in the query (deletions in the template).
+
+    # deletions are connected regions of no progress in the query index:
+    # alignment.
+    diff_index1 = alignment.index1[1:] - alignment.index1[0:-1]
+    # find the regions where this is zero
+    deletion_state = (diff_index1 == 0).astype(int)
+    # run connected components analysis
+    cc_deletions = measure.label(deletion_state, connectivity=1)
+    # scan the cc for region extents
+    reference_index = np.arange(0, len(cc_deletions))
+    deletion_regions = []
+    for region_label in range(1, max(cc_deletions) + 1):
+        deletion_region_indices = reference_index[cc_deletions == region_label]
+        # find the region indices in the alignment index (as opposed to
+        # the index in the index differences)
+        region_min = min(deletion_region_indices) + 1
+        region_max = max(deletion_region_indices) + 2
+        # compute the distance along this items in the query
+        pts = template[alignment.index2[region_min:region_max], :]
+        pts_diff = pts[1:, :] - pts[0:-1, :]
+        dist = np.sum(np.linalg.norm(pts_diff, axis=1))
+        # print(region_label, region_min, region_max, dist)
+        deletion_regions.append((region_min, region_max, dist))
+    # now copy the appropriate patches in the query and template into
+    # the result.
+    # First, copy in the entire query.
+    len_output = len(alignment.index1)
+    output = np.zeros(shape=(len_output, 2))
+    output = query[alignment.index1, :]
+    # and now fix the deletions. We already have the insertions.
+    # filter out the small regions
+    min_dist = template_dist * 0.02
+    for region in deletion_regions:
+        d = region[2]
+        if d > min_dist:
+            region_min = region[0]
+            region_max = region[1]
+            output[region_min:region_max, :] = template[region_min:region_max, :]
+    # print(output)
+    return output
+
+
+def gpx_to_lat_lon(file_name):
+    ''' see https://towardsdatascience.com/build-interactive-gps-activity-maps-from-gpx-files-using-folium-cf9eebba1fe7 '''
+    gf = open(file_name, 'r')
+    gfp = gp.parse(gf)
+    points = []
+    for track in gfp.tracks:
+        for segment in track.segments:
+            for point in segment.points:
+                points.append((point.latitude, point.longitude))
+    return points
+
+
 
 
 def patch_gaps(gpx1, gpx2, gpx_out, gap_threshold_meters, gap_threshold_seconds):
--- test/test_dtw.py
+++ test/test_dtw.py
@@ -2,6 +2,7 @@ import unittest
 import numpy as np
 import dtw
 import matplotlib.pyplot as plt
+import skimage.measure as measure
 
 
 class MyTestCase(unittest.TestCase):
@@ -42,10 +43,104 @@ class MyTestCase(unittest.TestCase):
         template[:, 1] = template_y
 
         alignment = dtw.dtw(query, template, keep_internals=True)
+
+        # compute distance along the template as a reference length
+        delta = template[1:, :] - template[0:-1, :]
+        template_dist = np.sum(np.linalg.norm(delta, axis=1))
+        #print(template_dist)
+
+        delta = query[1:, :] - query[0:-1, :]
+        query_dist = np.sum(np.linalg.norm(delta, axis=1))
+        #print(query_dist)
+
         # Display the warping curve, i.e. the alignment curve
         alignment.plot(type="threeway")
         # this does not work in higher dimensions!
         #alignment.plot(type="twoway")
+
+        # merge these two trajectories into a single trajectory.
+        # first, locate the alignment gaps. These have to be at least
+        # min_dist in euclidean dist to be corrected. We'll do this with
+        # connected component analysis of alignment regions which have zero
+        # progress in the query (deletions in the template).
+
+        # deletions are connected regions of no progress in the query index:
+        # alignment.
+        diff_index1 = alignment.index1[1:] - alignment.index1[0:-1]
+        # find the regions where this is zero
+        deletion_state = (diff_index1 == 0).astype(int)
+        # run connected components analysis
+        cc_deletions = measure.label(deletion_state, connectivity=1)
+        # scan the cc for region extents
+        reference_index = np.arange(0, len(cc_deletions))
+        deletion_regions = []
+        for region_label in range(1, max(cc_deletions)+1):
+            deletion_region_indices = reference_index[cc_deletions == region_label]
+            # find the region indices in the alignment index (as opposed to
+            # the index in the index differences)
+            region_min = min(deletion_region_indices)
+            region_max = max(deletion_region_indices)+1
+            # compute the distance along this items in the query
+            pts = template[alignment.index2[region_min:region_max+1], :]
+            pts_diff = pts[1:, :] - pts[0:-1, :]
+            dist = np.sum(np.linalg.norm(pts_diff, axis=1))
+            #print(region_label, region_min, region_max, dist)
+            deletion_regions.append((region_min, region_max, dist))
+        # insertions are connected regions of no progress in the template index:
+        diff_index2 = alignment.index2[1:] - alignment.index2[0:-1]
+        # find the regions where this is zero
+        insertion_state = (diff_index2 == 0).astype(int)
+        # run connected components analysis
+        cc_insertions = measure.label(insertion_state, connectivity=1)
+        # scan the cc for region extents
+        reference_index = np.arange(0, len(cc_insertions))
+        insertion_regions = []
+        for region_label in range(1, max(cc_insertions)+1):
+            insertion_region_indices = reference_index[cc_insertions == region_label]
+            # find the region indices in the alignment index (as opposed to
+            # the index in the index differences)
+            region_min = min(insertion_region_indices)
+            region_max = max(insertion_region_indices)+1
+            # compute the distance along this items in the query
+            pts = query[alignment.index1[region_min:region_max+1], :]
+            pts_diff = pts[1:, :] - pts[0:-1, :]
+            dist = np.sum(np.linalg.norm(pts_diff, axis=1))
+            #print(region_label, region_min, region_max, dist)
+            insertion_regions.append((region_min, region_max, dist))
+        # now copy the appropriate patches in the query and template into
+        # the result.
+        # First, copy in the entire query.
+        len_output = len(alignment.index1)
+        output = np.zeros(shape=(len_output, 2))
+        output = query[alignment.index1, :]
+        # and now fix the deletions. We already have the insertions.
+        # filter out the small regions
+        min_dist = template_dist * 0.05
+        for region in deletion_regions:
+            d = region[2]
+            if d > min_dist:
+                region_min = region[0]
+                region_max = region[1]
+                output[region_min:region_max, :] = template[region_min:region_max, :]
+        #print(output)
+
+        plt.figure()
+        plt.plot(output[:, 0], 'b+')
+        plt.plot(output[:, 1], 'r+')
+        plt.title('corrected query')
+        plt.show()
+
+        # compute distance along the output - this should be 5pi/2, except
+        # that the query is noisy, so it is query_dist + pi/2
+        delta = output[1:, :] - output[0:-1, :]
+        output_dist = np.sum(np.linalg.norm(delta, axis=1))
+        print('output_dist:', output_dist)
+        target_dist = query_dist + np.pi/2
+        print('target dist:', target_dist)
+        max_diff = target_dist * 0.05
+        self.assertTrue(abs(output_dist-target_dist) < max_diff)
+
+
 
 
 if __name__ == '__main__':
